06f0b1c23c01c9c000c083f98f0b84066ea1935d
Antoine Rose <antoine_rose@hotmail.fr>
2018-12-06 17:27:25 +0100

    fix bug

--- apps/beatitud_corpus/contrib/drf/bible/urls.py
+++ apps/beatitud_corpus/contrib/drf/bible/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import BibleVerseViews
 
 urlpatterns = [
-    url(r'^verse/$', BibleVerseViews.as_view({'get': 'list'}), name='verse'),
+    url(r'^verse/$', BibleVerseViews.as_view(), name='verse'),
     # url(r'apps/$', get_app_versions, name='reviews-apps'),
     # url(r'tags/$', get_tags, name='tags-reviews'),
 ]
